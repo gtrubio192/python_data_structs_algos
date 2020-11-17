30d23943ad49a9f5aec31f98af1773e47d3d6f36
Gabe Rubio <gtrubio192@gmail.com>
2020-11-16 20:07:33 -0700

    creating in order traversal of bst

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -11,28 +11,40 @@ class BSTDemo:
     def insert(self, key):
         if not isinstance(key, Node):
             key = Node(key)
+        # if Empty tree
         if self.root == None:
             self.root = key
+        # else, use private method
         else:
             self._insert(self.root, key)
 
+    # "_xyz() " functions are said to be private functions 
     def _insert(self, curr, key):
+        # deal with inserting on right
         if key.data > curr.data:
             if curr.right_child == None:
                 curr.right_child = key
             else:
                 self._insert(curr.right_child, key)
+        # deal with inserting on left
         elif key.data < curr.data:
             if curr.left_child == None:
                 curr.left_child = key
             else:
                 self._insert(curr.left_child, key)
 
+    # print left, root, then right node
     def in_order(self):
-        pass
+        self._in_order(self.root)
+        print(" ")
 
     def _in_order(self, curr):
-        pass
+        # go all the way to left most node recursively (print left, root, right)
+        if curr:
+            self._in_order(curr.left_child)
+            print(curr.data, end=" ")
+            self._in_order(curr.right_child)
+
 
     def pre_order(self):
         '''root, left, right'''
@@ -62,16 +74,10 @@ class BSTDemo:
 
 tree = BSTDemo()
 tree.insert("F")
-print(tree.root.data)
 tree.insert("C")
-print(tree.root.left_child.data)
 tree.insert("G")
-print(tree.root.right_child.data)
 tree.insert("A")
-print(tree.root.left_child.left_child.data)
 tree.insert("B")
-print(tree.root.left_child.left_child.right_child.data)
 tree.insert("K")
-print(tree.root.right_child.right_child.data)
 tree.insert("H")
-print(tree.root.right_child.right_child.left_child.data)
+tree.in_order()
